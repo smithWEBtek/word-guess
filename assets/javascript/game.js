99fc4aa3144c8e2777bac71febb36d91124dfd84
smithwebtek <brad@smithwebtek.com>
2018-04-24 12:00:00 -0400

    Update js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,37 @@
-
-// You're going to make a game just like the one in the video. 
-// Essentially, the app randomly picks a letter, 
-// and the user has to guess which letter the app chose.
-
-// Put the following text on your page:
-
-// Guess what letter I'm thinking of
-// Wins: (# of times the user has guessed the letter correctly)
-// Losses: (# of times the user has failed to guess the letter correctly 
-// after exhausting all guesses)
-// Guesses Left: (# of guesses left. This will update)
-// Your Guesses So Far: (the specific letters that the user typed. 
-// Display these until the user either wins or loses.)
-
-// When the player wins, increase the Wins counter and 
-// start the game over again (without refreshing the page).
-// When the player loses, increase the Losses counter and 
-// restart the game without a page refresh (just like when the user wins).
-
 var computerLetter = ''
 var userGuess = ''
-var wins = 0
+var wins = "asdf"
 var losses = 0
 var userGuessesLeft = 26
 var userGuessesSoFar = []
+var word = 'Alphabet'
+
+setWins = () => {
+	document.getElementById('wins').innerText = 'asdf'
+}
+
+setLosses = () => {
+	document.getElementById('losses').innerText = 'zxcv'
+}
+
+computerGuess = () => {
+	let range = word.length
+	guess = (range) => {
+		return Math.floor(Math.random() * Math.floor(range));
+	}
+	console.log(guess(range));
+}
+
+userGuess = () => {
+	console.log(document.onkeyup)
+}
 
 
 
+
+
+
+
+
+// setWins()
+// setLosses()
